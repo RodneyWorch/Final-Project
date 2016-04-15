98bc3e2adc684f414cf1658c69f7f04fea863d6c
RodneyWorch <rodney.worch@gmail.com>
2016-04-14 19:15:40 -0600

    Update myjavascript.js

--- myjavascript.js
+++ myjavascript.js
@@ -1,1 +1,20 @@
+// JavaScript Counter     
+function myCounter() {
+  return ++count;
+};
 
+// Javascript for loop using an Array 
+function myMusic() {
+  var index;
+  var text = "";
+
+  for (index = 0; index < music.length; index++) {
+    text += music[index] + "<br />";
+  }
+  document.getElementById("demo").innerHTML = text;
+};
+
+// Javascript for random number generator 
+function myFunction() {
+  document.getElementById("demo1").innerHTML = Math.random();
+};
