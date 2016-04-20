cc327a37e7cabaec784bb58332e721c7ad7e94df
RodneyWorch <rodney.worch@gmail.com>
2016-04-20 17:24:16 -0600

    Update myjavascript.js

--- myjavascript.js
+++ myjavascript.js
@@ -1,20 +1,12 @@
-// JavaScript Counter     
-function myCounter() {
-  return ++count;
-};
-
-// Javascript for loop using an Array 
-function myMusic() {
-  var index;
-  var text = "";
-
-  for (index = 0; index < music.length; index++) {
-    text += music[index] + "<br />";
-  }
-  document.getElementById("demo").innerHTML = text;
-};
-
-// Javascript for random number generator 
-function myFunction() {
-  document.getElementById("demo1").innerHTML = Math.random();
-};
+$(function() {
+    $('.kc-wrap').KillerCarousel({
+        // Width of carousel.
+        width: 800, 
+        // Item spacing in 3D (modern browsers) modes.
+        spacing3d: 120,    
+        // Item spacing in 2D (old browsers) modes. 
+        spacing2d: 120,
+        showShadow:true,
+        showReflection: true
+    });
+});
