77bd3b238ee9d885a4f0a644194e3f30143f65ed
RodneyWorch <rodney.worch@gmail.com>
2016-04-22 19:14:55 -0600

    Create carousel.js

---
+++ carousel.js
@@ -0,0 +1,17 @@
+function animate(opts){
+    var start = new Date;
+    var id = setInterval(function(){
+        var timePassed = new Date - start;
+        var progress = timePassed / opts.duration
+        if(progress > 1){
+            progress = 1;
+        }
+        var delta = opts.delta(progress);
+        opts.step(delta);
+        if (progress == 1){
+            clearInterval(id);
+           opts.callback();
+         }
+    }, opts.dalay || 17);
+}
+window.onload = init;
